8dad5fc25632ae435ac38b770ee19f8a4d25e9e8
Evgenii Panov <e.panov@addstore.com>
2016-02-09 19:13:13 +0200

    config file

--- 10-create-disk.sh
+++ 10-create-disk.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 
-##	Disk size in Mb
-size_mb=512
+dir="$( cd "$( dirname "$0" )" && pwd )"
+. $dir/00-config.conf
 
-host_path="$( cd "$( dirname "$0" )" && pwd )"
-
-##	Path to host disk
-fs_path=$host_path/disk.fs
-
-##	Mount point path
-mount_path=$host_path/disk
-
-
+##	Function for creating disk
+##
+##	Params:
+##	$1	$fs_path
+##	$2	$size_mb
+##
 create_disk () {
 	echo "START: Create disk $1 with size: $2 Mb"
 	dd if=/dev/zero of=$1 bs=1024 count=$(($2*1024))
 	echo "DONE: Create disk"
 }
 
+##	Function for formatting disk
+##
+##	Params:
+##	$1	$fs_path
+##
 format_disk () {
 	echo "START: Format disk $1"
 	yes "yes" | mkfs.ext3 $1
 	echo "DONE: Format disk"
 }
 
+##	Function for mounting disk
+##
+##	Params:
+##	$1	$fs_path
+##	$2	$mount_path
+##
 mount_disk () {
 	echo "START: Mount disk $1 to $2"
 	mkdir -p $2
@@ -31,7 +39,13 @@ mount_disk () {
 	echo "DONE: Mount disk"
 }
 
-function auto_mount {
+##	Function for auto mounting disk after reboot
+##
+##	Params:
+##	$1	$fs_path
+##	$2	$mount_path
+##
+auto_mount () {
 	if ! grep -q "$1 $2" /etc/fstab; then
 		echo "$1 $2 ext3 loop 1 2" >> /etc/fstab
 	fi
